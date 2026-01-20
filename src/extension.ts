8c252018bffb3fcba6195703a5958a261337e29c
X2002w <15008695828@163.com>
2026-01-20 20:04:34 +0800

    init riscv-assembly project repository

---
+++ src/extension.ts
@@ -0,0 +1,6 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Extension "my-extension" is now active! ');
+    // Your extension activation code goes here
+}
